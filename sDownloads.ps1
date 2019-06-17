f893bf9c367e152435f4a476b6df43edf89e713b
David King <dxking16@gmail.com>
2019-06-16 18:02:52 -0700

    fixed path issues

--- sDownloads.ps1
+++ sDownloads.ps1
@@ -1,6 +1,9 @@
-﻿# sd-config.csv needs to stay with sDownloads.ps1
+﻿# Check directory the directory sDownloads.ps1 runs from for sd-config file
+$ConfigPath = $PSScriptRoot + "\sd-config"
+
+# sd-config needs to stay with sDownloads.ps1
 # or change the path if you want sd-config.csv somewhere else
-$Config = Import-Csv -Path .\sd-config.csv
+$Config = Import-Csv -Path $ConfigPath
 
 # Will work fine as long as this is the folder you use for downloads
 $Downloads = "C:\Users\$env:UserName\Downloads\"
@@ -12,8 +15,13 @@ ForEach($e in $Config) {
     ForEach($f in $FileNames) {
         $FilePath = $Downloads + $f
         $FileExtension = [IO.Path]::GetExtension($f)
-        if($FileExtension -eq $e.Extension){
+
+        if($FileExtension -eq $e.Extension) {
+            # Create destination folder if it doesn't exist
+            if(!(Test-Path $e.Path -PathType Container)) {
+                New-Item -Path $e.Path -ItemType Directory 
+            }
             Move-Item -Path $FilePath -Destination $e.Path
         }    
     }
-  }
+}
